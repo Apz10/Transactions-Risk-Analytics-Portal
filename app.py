301c5a01e8859473971f8fbd874d6fb2acb654de
Apz <sapoorv500@gmail.com>
2025-10-02 07:29:19 +0200

    Added files

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+import json
 
 # Sidebar navigation
 st.sidebar.title("Transactions Risk Analytics Portal")
-page = st.sidebar.radio("Go to", ["Welcome", "Counter Party Analysis", "Data Counter Party Analysis"])
+page = st.sidebar.radio("Go to", ["Welcome", "Counter Party Analysis", "Data Counter Party Analysis", "Training Data Generation"])
 
 # Welcome Page
 if page == "Welcome":
@@ -28,7 +29,12 @@ if page == "Welcome":
 					- Improper – Possible Tax Reduction
 					- Recurring Client Sales
 					- Unusual – Third Party
+			 Finally, we provide insights into potential risks and anomalies in transaction data.
 				""")
+	st.markdown("Trained on small dataset, large training in-progress :)")
+	ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+	# st.write(os.path.join(ROOT_DIR, "Transactions-Risk-Analytics-Portal/webresource/images/TrainingGraph.png"))
+	st.image(os.path.join(ROOT_DIR, "Transactions-Risk-Analytics-Portal/webresource/images/TrainingGraph.png"), caption="", use_container_width =True)
 
 
 # Counter Party Analysis Page
@@ -58,23 +64,29 @@ elif page == "Counter Party Analysis":
 	if st.button("Submit CSV for Risk Analysis"):
 		# Step3f: Send to OpenAI model
 		try:
-			prompt = f'''Detect suspicious transactions in the following data:\n,  
-			choose scenario from the following list:
-			\n- Business Income – Export
-			\n- Business Income – Domestic
-			\n- Business Expense – Supplier
-			\n- Regulatory Expense
-			\n- Private Investment Company – Related Party
-			\n- Improper – Possible Tax Reduction
-			\n- Recurring Client Sales
-			\n- Unusual – Third Party''' 
 
-			prompt += edited_df.to_csv(index=False)
-
-			result = cp_analysis_agent.run_counterparty_analysis(prompt, llm_agent)
-			# Step3g: Display result
-			st.subheader("Model Output")
-			st.write(result)
+			prompt = f'''Label all the transaction data with one of the scenario Business Income Export,Business Income Domestic,Business Expense Supplier,Regulatory Expense,Private Investment Company Related Party,Improper Possible Tax Reduction,Recurring Client Sales,Unusual Third Party, only output the scenario and transaction id.
+			{edited_df.to_csv(index=False)}
+			'''
+			if llm_agent.startswith("ft:"):
+				st.subheader("Fine-tuned Model Output")
+				aggregated_results = edited_df.groupby('CounterpartyName')['AmountEUR'].sum().reset_index()
+				counterparty_list = edited_df['TxnID'].tolist()
+				result = cp_analysis_agent.run_counterparty_analysis(prompt, "gpt-4")
+				sus_cp = []
+				for i in counterparty_list:
+					if i in result:
+						sus_cp.append(i)
+				st.write("Suspicious Counterparties to look into:")
+				sus_cp_df = pd.DataFrame(sus_cp, columns=["Suspicious_TxnId"])
+				result_sus = pd.merge(edited_df, sus_cp_df, left_on='TxnID', right_on='Suspicious_TxnId', how='inner')
+				st.write(result_sus)
+				st.write(result)
+			else:
+				result = cp_analysis_agent.run_counterparty_analysis(prompt, llm_agent)
+				st.subheader("Model Output")
+				# Step3g: Display result
+				st.write(result)
 		except Exception as e:
 			st.error(f"No file submitted")
 			st.markdown("Error from Agent Run:")
@@ -95,6 +107,33 @@ elif page == "Data Counter Party Analysis":
 	df = pd.read_csv(os.path.join(ROOT_DIR, "test/data/transactions_generated_scenarios_gpt5.csv"))
 	edited_df = st.data_editor(df, num_rows="dynamic")
 
+# Data Counter Party Analysis
+elif page == "Training Data Generation":
+	st.title("Training Data For Counter Party Analysis")
+
+	st.markdown("Training JSONL Data for Counter Party Analysis:")
+	ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+	with open(os.path.join(ROOT_DIR, "test/data/transactions_finetune.jsonl"), "r", encoding="utf-8") as f:
+		for line in f:
+			record = json.loads(line)
+			print(record)
+	st.write(record)
+
+	st.markdown("Validation JSONL Data for Counter Party Analysis:")
+	with open(os.path.join(ROOT_DIR, "test/data/transactions_training.jsonl"), "r", encoding="utf-8") as f:
+		for line in f:
+			record = json.loads(line)
+			print(record)
+	st.write(record)
+
+	st.markdown("Validation JSONL Data for Counter Party Analysis:")
+	with open(os.path.join(ROOT_DIR, "test/data/transactions_validation.jsonl"), "r", encoding="utf-8") as f:
+		for line in f:
+			record = json.loads(line)
+			print(record)
+	st.write(record)
+
 st.markdown(
     """
     <style>
@@ -110,7 +149,7 @@ st.markdown(
         user-select: none;
     }
     </style>
-    <div class="watermark">v.1.0.2</div>
+    <div class="watermark">v.1.0.3</div>
     """,
     unsafe_allow_html=True
 )
